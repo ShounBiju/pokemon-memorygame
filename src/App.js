961200850691a9e7608fc973d7a32266cc13ccc9
ShounBiju <shounbijumcpe@gmail.com>
2024-11-20 16:19:36 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,81 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import PokemonLogo from './Assets/PokemonLogo.png';
 import ButtonStyle from './Components/ButtonStyle';
+import SingleCard from './Components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/charmander-1.jpeg" },
-  { "src": "/img/jigglypuff-1.jpeg" },
-  { "src": "/img/pikachu-1.jpeg" },
-  { "src": "/img/meowth-1.webp" },
-  { "src": "/img/poison-1.jpeg" },
-  { "src": "/img/Snorelax-1.jpeg" }
+  { "src": "/img/jigglypuf-1.jpg", matched: false },
+  { "src": "/img/meowth-1.jpg", matched: false },
+  { "src": "/img/meowtwo-1.jpg", matched: false },
+  { "src": "/img/pikachu-1.jpg", matched: false },
+  { "src": "/img/psyduck-1.jpg", matched: false },
+  { "src": "/img/snorelax-1.jpg", matched: false }
 ];
 
 function App() {
   const [cards, setCards] =useState([])
   const [turns, setTurns] = useState(0)
-
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+  //shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [ ...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
-
-      setCards(shuffledCards)
-      setTurns(0)
+      
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards)
+    setTurns(0)
   };
+  //handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
 
-  console.log(cards, turns)
+  }
+
+
+  //compare 2 selected cards
+  useEffect(() => {
+    
+
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  },[choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  //reset choises and increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  //start game automatically
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
   return (
     <div className="App">
       <img
@@ -34,10 +84,22 @@ function App() {
         className="logo"
       />
       <div className="button-container">
-        {/* Pass the onClick prop to ButtonStyle */}
         <ButtonStyle onClick={shuffleCards} />
         <p className="play-text">Play Game</p>
       </div>
+
+      <div className='card-grid'>
+        {cards.map(card => (
+          <SingleCard 
+            key={card.id} 
+            card={card} 
+            handleChoice={handleChoice} 
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
---
+++ src/Components/SingleCard.js
@@ -0,0 +1,22 @@
+import './SingleCard.css'
+
+function SingleCard({card, handleChoice, flipped, disabled}) {
+
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card)
+    }
+    
+  }
+
+  return (
+    <div className='card'>
+        <div className={flipped ? "flipped" : ""}>
+            <img className='front' src={card.src} alt="card front"/>
+            <img className='back' src="/img/pokeball.jpg" onClick={handleClick} alt="card back"/>
+        </div>
+    </div>
+  )
+}
+
+export default SingleCard
